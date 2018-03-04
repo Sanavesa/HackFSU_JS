3809a6ff4ff8d26fd487171ffb5df89a11b87cd1
Mohammad Alali <bbqsauced2@gmail.com>
2018-03-03 22:37:58 -0500

    Init2

---
+++ my-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import HomePage from './components/HomePage'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
---
+++ my-project/src/components/HomePage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <button v-on:click="requestPost">Post!</button>
+    <button v-on:click="requestGet">Get!</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: 'Welcomes!'
+    }
+  },
+  methods: {
+    requestGet: function(event) {
+      let url = "https://hackfsu18.herokuapp.com/get-data";
+
+      axios.get(url, {
+          method: 'GET',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-type': 'application/json'
+          },
+      }).then(response => console.log(response));
+    },
+    requestPost: function(event) {
+      let obj = {
+          "lecture": "MyAwesomeness",
+          "course": "CS 420",
+          "professor": "Prof. Robinson",
+          "category": "AWESOME",
+          "text": "some text that i love to decipher and summarize"
+      };
+
+      let jsonified = JSON.stringify(obj);
+      let url = "https://hackfsu18.herokuapp.com/send-full-text";
+
+      axios.post(url, obj, {
+          method: 'POST',
+          // baseURL: 'https://hackfsu18.herokuapp.com/',
+          // url: '/send-full-text'
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          //   'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+            'Content-type': 'application/json'
+          },
+          // withCredentials: true,
+          // credentials: 'same-origin',
+      }).then(response => console.log(response));
+    }
+  }
+}
+</script>
+<style>
+h1 {
+  font-weight: bold;
+}
+</style>
